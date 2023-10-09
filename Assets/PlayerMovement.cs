56b44148b86c03b04773a86724f981f1502e76fb
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2023-10-09 17:09:15 +0300

    Add files via upload

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+    }
+}
